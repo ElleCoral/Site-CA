6218c3f138e4c5a863b769001ac8356295c71011
ElleCoral <gabrielle.coral@hotmail.com>
2023-11-28 21:09:17 -0300

    ola

---
+++ JavaScript/style.js
@@ -0,0 +1,13 @@
+var abrirMenu
+var fecharMenu
+
+function abrirMenu(){
+
+    document.getElementById('menu1').style.width='250px'
+}
+
+function fecharMenu(){
+    
+    document.getElementById('menu1').style.width='0px'
+
+}
